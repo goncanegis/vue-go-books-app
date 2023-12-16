04daa23a5716223539f73105a017af7d819ef149
gonca <gonca.negis@gmail.com>
2023-12-17 00:07:57 +0300

    add success, error emits to router views

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <TheHeader />
 
-  <RouterView />
+  <RouterView @success="success" @error="error" @warning="warning" />
 
   <TheFooter />
 </template>
@@ -12,12 +12,34 @@ import TheHeader from './components/TheHeader.vue'
 import TheFooter from './components/TheFooter.vue'
 import { store } from './components/store'
 import { nextTick, onBeforeMount, onMounted } from 'vue'
+import notie from 'notie'
 
 const getCookie = (name: string) => {
   return document.cookie.split('; ').reduce((r, v) => {
     const parts = v.split('=')
     return parts[0] === name ? decodeURIComponent(parts[1]) : r
   }, '')
+}
+
+const success = (message: string) => {
+  notie.alert({
+    type: 'success',
+    text: message
+  })
+}
+
+const error = (message: string) => {
+  notie.alert({
+    type: 'error',
+    text: message
+  })
+}
+
+const warning = (message: string) => {
+  notie.alert({
+    type: 'warning',
+    text: message
+  })
 }
 
 onBeforeMount(() => {
